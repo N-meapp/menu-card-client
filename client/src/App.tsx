7f640f2d3683829d7a62065563e64f366c0b1edb
N-meapp <developersnme@gmail.com>
2024-09-28 11:18:16 +0530

    updated app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import { useState } from "react";
 import LoginContext from "./components/loginContext";
