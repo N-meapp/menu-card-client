22952865221f4fea70e910b99ea3b3b1fd2ee21c
N-meapp <developersnme@gmail.com>
2024-09-28 11:13:49 +0530

    hosting on render

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import React from 'react'
-import LoginContext from './components/loginContext.tsx'
 
 
 
